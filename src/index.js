1f6dad863a25c4a527b8b2d42da54f3ce1bb10d8
Aman Singh <amansingh979429@gmail.com>
2025-12-24 21:17:07 +0530

    data base connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const { PORT } = require("./config");
+const { connectDB } = require("./config/db");
 const app = express();
+
+// DB connection
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`------------ Server Started At ${PORT} ------------`);
