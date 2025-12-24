688fd7ab250eaa7fcb67b2e31fb83d18a849eb5b
Aman Singh <amansingh979429@gmail.com>
2025-12-24 21:42:02 +0530

    route and model setup

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const { PORT } = require("./config");
 const { connectDB } = require("./config/db");
 const app = express();
+const apiRoutes = require("./routes");
 
 // DB connection
 connectDB();
+
+// Models
+require("./models");
+
+//Routes
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`------------ Server Started At ${PORT} ------------`);
--- src/routes/v1/auth.js
+++ src/routes/v1/auth.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const v1Routes = require("./v1");
 
-router.use("/v1", v1Routes);
+router.post("/signup", () => {});
 
 module.exports = router;
