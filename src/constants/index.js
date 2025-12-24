edbab7c0b1211db5a10115fa5203e0f5fd49427e
Aman Singh <amansingh979429@gmail.com>
2025-12-24 21:11:52 +0530

    project init

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+const USER_ROLES = {
+  STUDENT: "student",
+  TEACHER: "teacher",
+};
+
+const ENV = {
+  DEV: "dev",
+  PRODUCTION: "production",
+};
+
+module.exports = {
+  USER_ROLES,
+  ENV,
+};
---
+++ src/models/users.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const { USER_ROLES } = require("../constants");
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: Object.values(USER_ROLES),
+      default: USER_ROLES.USER,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+UserSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+UserSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("users", UserSchema);
